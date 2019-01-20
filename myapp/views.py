57ea14fc6f3e0f773e9af112891c1809d21df2eb
Sankusu <sankususharma@gmail.com>
2019-01-20 13:33:48 +0530

    added html page

--- myapp/views.py
+++ myapp/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+
+# def index(request):
+#     return HttpResponse("Hello World!!")
+
 def index(request):
-    return HttpResponse("Hello World!!")
+    return render(request, "myapp/index.html")
